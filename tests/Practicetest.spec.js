1a85056cc265ce0c21b5712c54fa63d3564ccab6
Sunil <sunilpatil1308@gmail.com>
2025-04-25 12:38:32 +0530

    Flipkart Code

--- tests/Practicetest.spec.js
+++ tests/Practicetest.spec.js
@@ -1,8 +1,23 @@
-exports.class=class GitHub
-{
+const {test,expect}= require("@playwright/test");
 
-    contructor()
-    {
+test("test", async({page})=>{
 
-    }
-}
+
+    await page.goto("https://www.flipkart.com/");
+
+
+    const Links =await page.$$("._1XjE3T")//.textContent()
+
+    const categories = [];
+
+   for(const link of Links)
+   {
+       const textContent = await link.textContent()
+
+       if (textContent) {
+        categories.push(textContent.trim());
+      
+   }
+   }
+   console.log(categories)
+})
