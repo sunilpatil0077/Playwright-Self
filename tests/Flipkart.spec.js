38513a8dfd579bf13fc019c16e819462c63f20b9
Sunil <sunilpatil1308@gmail.com>
2025-04-25 14:49:28 +0530

    New Test

--- tests/Flipkart.spec.js
+++ tests/Flipkart.spec.js
@@ -1,6 +1,6 @@
 const {test,expect}= require("@playwright/test");
 
-test("test", async({page})=>{
+test("Print All Names", async({page})=>{
 
 
     await page.goto("https://www.flipkart.com/");
@@ -21,4 +21,22 @@ test("test", async({page})=>{
    }
    console.log(categories)
    
+})
+
+test("Open RC Website", async({page})=>{
+
+
+    await page.goto("https://www.royalcanin.com/in");
+
+    const menu=await page.locator('.sc-a6dbebca-1.hRThxe')
+    await expect(menu).toHaveCount(7)
+    const CTA=await page.locator("//div[@class='sc-7217a029-1 bvnhQm']//a[@title='Get the tailored product'][normalize-space()='Get the tailored product']").nth(0)
+    await CTA.click();
+    await page.waitForTimeout(4000)
+    await expect(page).toHaveURL("https://www.royalcanin.com/in/pet-food-finder")
+
+
+
+
+    //await page.pause()
 })
